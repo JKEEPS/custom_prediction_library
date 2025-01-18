b12d31bd1b22ece731a7c8c8bd43e88b7e4c4469
JKEEPS <107138788+JKEEPS@users.noreply.github.com>
2025-01-18 01:57:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="custom_prediction_lib",
+    name="custom_prediction_library",
     version="0.3.0",
     packages=find_packages(),
     install_requires=[
